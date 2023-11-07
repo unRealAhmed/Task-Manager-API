f70b82eeba36b1e864a1b4c99366db1506dbc611
Ahmed Sayed <therealahme@gmail.com>
2023-11-07 23:43:05 +0200

    improve the controllers

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ require("dotenv").config({ path: "./config.env" });
 const express = require('express')
 const taskRouter = require("./routes/taskRoutes");
 
+
+//Middleware
+const app = express()
+app.use(express.json())
+//Routes
+app.use('/api/v1/tasks', taskRouter)
+
 //DataBase Connection
 const connectDatabase = require("./utilities/dataBase");
 
 connectDatabase()
 
-//Middleware
-const app = express()
+//Starting the server
 const port = process.env.PORT || 8000
 
-//Routes
-app.use('/api/v1/tasks', taskRouter)
-
-//Starting the server
 app.listen(port, () => {
   console.log(`Server Listening To Requests On Port ${port}....`);
 })
--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,15 +1,57 @@
-exports.getAllTasks = (req, res) => {
-  res.send('All Tasks Here')
-}
-exports.getTask = (req, res) => {
-  res.send('Task Here')
-}
-exports.createTask = (req, res) => {
-  res.send('New Task Here')
-}
-exports.updateTask = (req, res) => {
-  res.send('Updated Task Here')
-}
-exports.deleteTask = (req, res) => {
-  res.send('deleted Task Here')
-}
+const Task = require('../models/taskModel')
+const asyncHandler = require('../utilities/asyncHandler')
+
+
+exports.getAllTasks = asyncHandler(async (req, res) => {
+
+  const tasks = await Task.find()
+
+  res.status(200).json({
+    status: 'success',
+    results: tasks.length,
+    tasks
+  })
+})
+
+exports.getTask = asyncHandler(async (req, res) => {
+
+  const task = await Task.findById(req.params.id)
+
+  res.status(200).json({
+    status: 'success',
+    task
+  })
+})
+
+exports.createTask = asyncHandler(async (req, res) => {
+
+  const newTask = await Task.create(req.body)
+
+  res.status(201).json({
+    status: 'success',
+    newTask
+  })
+})
+
+exports.updateTask = asyncHandler(async (req, res) => {
+
+  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  })
+
+  res.status(200).json({
+    status: 'success',
+    updatedTask
+  })
+})
+
+exports.deleteTask = asyncHandler(async (req, res) => {
+
+  await Task.findByIdAndDelete(req.params.id)
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+})

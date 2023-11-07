2fb03de6bed7e2602bd4da56fa1227870f909080
Ahmed Sayed <therealahme@gmail.com>
2023-11-07 21:53:38 +0200

    Connect to database

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ require("dotenv").config({ path: "./config.env" });
 const express = require('express')
 const taskRouter = require("./routes/taskRoutes");
 
+//DataBase Connection
+const connectDatabase = require("./utilities/dataBase");
+
+connectDatabase()
+
+//Middleware
 const app = express()
 const port = process.env.PORT || 8000
 
-//routes
-
+//Routes
 app.use('/api/v1/tasks', taskRouter)
 
 //Starting the server
